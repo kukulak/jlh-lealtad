732ab527ce0aee5b64a64060619c7a173fb52f6a
Christian Valderrama <christian@Christians-Mac-Studio.local>
2024-06-19 03:01:55 -0600

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-installGlobals();
+installGlobals()
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-});
+  build: {
+    outDir: 'build/client',
+    rollupOptions: {
+      output: {
+        // Define the chunks output
+      }
+    }
+  },
+  plugins: [remix(), tsconfigPaths()]
+})
